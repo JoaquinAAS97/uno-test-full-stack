e35c83a95ebd420e184598818d8fe80a5d200e0b
Joaquín Astorga Sáez <110950511+JoaquinAAS97@users.noreply.github.com>
2026-01-15 18:57:59 -0300

    Update Cards.tsx

--- frontend/app/game/menu/play/components/Cards.tsx
+++ frontend/app/game/menu/play/components/Cards.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import { CardProps } from "../interfaces/cards.interface";
 
 export function Cards({ title, url_img, flipped, onClick }: CardProps) {
+  const isValidUrl = url_img?.startsWith("http");
+
   return (
     <div
       onClick={onClick}
@@ -15,13 +17,14 @@ export function Cards({ title, url_img, flipped, onClick }: CardProps) {
         lg:w-32 lg:h-40
       "
     >
-      {flipped ? (
+      {flipped && isValidUrl ? (
         <>
           <Image
             src={url_img}
             alt={title}
-            width={128}   
-            height={160} 
+            width={128}
+            height={160}
+            unoptimized 
             className="w-full h-20 sm:h-24 md:h-28 lg:h-32 object-cover rounded"
           />
           <p className="text-[10px] sm:text-[12px] md:text-[14px] font-bold text-black text-center mt-1 leading-tight">
